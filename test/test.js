70d13ce37dbd7255732746d95b0afc7a76bc9c8a
elchaibentov <elhcaibentov1@gmail.com>
2017-06-17 08:10:06 +0300

    GREEN: adding code to cash.js

--- test/test.js
+++ test/test.js
@@ -1,11 +1,6 @@
-var assert = require("assert"); // core module
-var C = require('../cash.js');  // our module
-
-describe('Cash Register', function(){
-  describe('Module C', function(){
-    it('should have a getChange Method', function(){
-      assert.equal(typeof C, 'object');
-      assert.equal(typeof C.getChange, 'function');
-    })
-  })
-});  
+var C = {};                    // C Object simplifies exporting the module
+C.getChange = function () {    // enough to satisfy the test
+    'use strict';
+    return true;               // also passes JSLint
+};
+module.exports = C;            // export the module with a single method
