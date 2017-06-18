4765ea092df196d40b76d8c92448c6cd0c620108
elchaibentov <elhcaibentov1@gmail.com>
2017-06-18 12:57:15 +0300

    RED: lazy method failure after changing test.js

---
+++ cash.js
@@ -0,0 +1,6 @@
+var C = {};                    // C Object simplifies exporting the module
+C.getChange = function (totalPayable, cashPaid) {
+    'use strict';
+    return [50, 20, 20];    // just enough to pass :-)
+};
+module.exports = C;            // export the module with a single method
