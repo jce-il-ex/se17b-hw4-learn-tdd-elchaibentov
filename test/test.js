237b7b727a14ae83b95150012df732f18087baa7
elchaibentov <elhcaibentov1@gmail.com>
2017-06-18 13:06:30 +0300

    GREEN: realizing that all tests should be together in test.js

--- test/test.js
+++ test/test.js
@@ -13,6 +13,10 @@ describe('Cash Register', function(){
 it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
     assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
 })
+totalPayable = 1487                                 // £14.87  (fourteen pounds and eighty-seven pence)
+cashPaid     = 10000                                // £100.00 (one hundred pounds)
+dfference    = 8513                                 // £85.13
+change       = [5000, 2000, 1000, 500, 10, 2, 1 ]   // £50, £20, £10, £5, 10p, 2p, 1p
 it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
     assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
 });
