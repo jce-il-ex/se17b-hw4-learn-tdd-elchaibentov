ef1e5c464aa8f19220b72bce3eccc2cc57f6a907
elchaibentov <elhcaibentov1@gmail.com>
2017-06-18 12:53:27 +0300

    RED: changing main function in test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
-var C = {};                    // C Object simplifies exporting the module
-C.getChange = function () {    // enough to satisfy the test
-    'use strict';
-    return true;               // also passes JSLint
-};
-module.exports = C;            // export the module with a single method
+var assert = require("assert"); // node.js core module
+
+describe('Array', function(){
+  var C = require('../cash.js');  // our module
+it('getChange(210,300) should equal [50,20,20]', function(){
+    assert.deepEqual(C.getChange(210,300), [50,20,20]);
+})
+});
